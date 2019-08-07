602b0e941b3e3ab6a9b3aaa5c63456c102cc73d5
Yongguang Zhu <Yongguang.Zhu@microsoft.com>
2019-08-07 14:12:58 +0800

    Upgrade nuget; update version; add migration for IdSvr

--- src/ConfigService/Migrations/20190807061127_Upgrade-IdSvr252.Designer.cs
+++ src/ConfigService/Migrations/20190807061127_Upgrade-IdSvr252.Designer.cs
@@ -4,14 +4,16 @@ using ConfigService;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ConfigService.Migrations
 {
     [DbContext(typeof(ServiceDbContext))]
-    partial class ServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190807061127_Upgrade-IdSvr252")]
+    partial class UpgradeIdSvr252
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
